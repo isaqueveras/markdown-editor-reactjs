934a500e7a56c78054e42d40afc339892c155c21
IsaqueVeras <isaque.verasccb@gmail.com>
2021-04-01 18:13:18 -0300

    Markdown Editor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
 import './App.css';
 
 function App() {
+  const [markdown, setMarkdown] = useState('# Markdown editor: created by isaque Véras');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="markdown__container">
+      <textarea value={markdown} onChange={e => setMarkdown(e.target.value)} />
+    
+      <ReactMarkdown source={markdown} className="markdown__preview" />
     </div>
   );
 }
-
 export default App;
+
